b86d088856c9b3b4677c63e65eed206771c89c5d
Lidan Nissim <nissimlidan@gmail.com>
2023-05-11 08:12:22 +0300

    first lesson - homework

--- venv/main.py
+++ venv/main.py
@@ -1,37 +1,27 @@
+#ASSIGNMENT LESSSON 1 - 10/05/23
+# (1)
+a = 7
+b = 44.3
+print(a+b)
+print(a*b)
+print(b/a)
 
-def tic_tac_toe_choose():
-    acceptable_values = range(0,10)
-    within_range = False
-    user_input = 'test'
-    while (user_input.isdigit() == False or within_range == False):
-        #user_input = input('You entered an incorrect value, please try again, Please enter number between(0-9)')
-        user_input = input('Please enter number between(0-9)')
-        if user_input.isdigit() == False:
-            print('You entered an incorrect value, please try again')
-        if user_input.isdigit() == True:
-            if(int(user_input) in acceptable_values):
-                within_range = True
-            else:
-                print('You entered an value that not in range of 0-9, please try again')
-                print('test')
+# (2)
+# #a,b,c will be ... at the end
+# a = 9, b = 8, c = 17
 
+# (3)
+# There is no difference between "john" and 'john', both strings
 
+# (4)
+# In this way only strings can be added, if we want to print INT we will have to use the f format
+my_number = 5+5
+print(f'result is: {my_number}')
 
-tic_tac_toe_choose()
+# (5)
+# As soon as you put flout into int, the number will become a number without a decimal point, so the answer is 7(5+2)
 
-
-# row1 = ['_', '_', '_']
-# row2 = ['_', '_', '_']
-# row3 = ['_', '_', '_']
-#
-# position_row_input = input("Choose an row number from 0 to 2")
-# position_row_input = int(position_row_input)
-# for i in range(0,1):
-#     row1[position_row_input] = 'X'
-#     print(row1)
-#     print(row2)
-#     print(row3)
-
-#position_row_input = input("Choose an row number from 0 to 2")
-#position_col_input = input("Choose an colume number from 0 to 2")
-#print(position_row_input , position_col_input)
+# (6)
+a = 8
+b = "123"
+print(a + int(b))
